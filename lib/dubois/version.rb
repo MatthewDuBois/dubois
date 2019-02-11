868286d3937cfba964bf76066fc64f8b89853642
Matthew DuBois <mattrdubois@gmail.com>
2019-02-11 15:20:47 -0600

    creating a gem for the first time

---
+++ lib/dubois/version.rb
@@ -0,0 +1,3 @@
+module Dubois
+  VERSION = "0.1.0"
+end
---
+++ spec/dubois_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Dubois do
+  it "has a version number" do
+    expect(Dubois::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
